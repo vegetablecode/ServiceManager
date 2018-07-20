ae33175d8ae305cb1d673664161c7efc1dfb1683
Kamil <kamil4525@gmail.com>
2018-07-20 21:16:50 +0200

    more fields

--- lib/pages/NewClient.dart
+++ lib/pages/NewClient.dart
@@ -15,7 +15,6 @@ class NewClientWidget extends StatefulWidget {
 }
 
 class NewClientState extends State<NewClientWidget> {
-
   TextEditingController nazwaKlienta = new TextEditingController();
   TextEditingController nip = new TextEditingController();
 
@@ -28,12 +27,65 @@ class NewClientState extends State<NewClientWidget> {
   @override
   Widget build(BuildContext context) {
     return new Center(
-        child: new Column(
+        child: new ListView(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(20.0),
       children: <Widget>[
-        new MyCard(label: "Nazwa klienta: ", controller: nazwaKlienta,),
-        new MyCard(label: "NIP klienta: ", controller: nip,),
-        new MyCard(label: "Okres umowy: ", controller: nip,),
-        new MyCard(label: "Liczba darmowych kopii: ", controller: nip,),
+        new Text(
+          "Dane klienta",
+          textAlign: TextAlign.center,
+          style: new TextStyle(fontSize: 20.0),
+        ),
+        new MyCard(
+          label: "Nazwa klienta: ",
+          controller: nazwaKlienta,
+        ),
+        new MyCard(
+          label: "NIP klienta: ",
+          controller: nip,
+        ),
+        new MyCard(
+          label: "Okres umowy: ",
+          controller: nip,
+        ),
+        new MyCard(
+          label: "Ryczałt miesięczny: ",
+          controller: nip,
+        ),
+        new Padding(padding: new EdgeInsets.only(bottom: 20.0)),
+        new Text(
+          "Szczegóły umowy",
+          textAlign: TextAlign.center,
+          style: new TextStyle(fontSize: 20.0),
+        ),
+        new MyCard(
+          label: "Rodzaj urządzenia: ",
+          controller: nip,
+        ),
+        new MyCard(
+          label: "Liczba darmowych kopii: ",
+          controller: nip,
+        ),
+        new MyCard(
+          label: "Liczba darmowych kopii: ",
+          controller: nip,
+        ),
+        new MyCard(
+          label: "Liczba darmowych kopii: ",
+          controller: nip,
+        ),
+        new MyCard(
+          label: "Liczba darmowych kopii: ",
+          controller: nip,
+        ),
+        new MyCard(
+          label: "Liczba darmowych kopii: ",
+          controller: nip,
+        ),
+        new MyCard(
+          label: "Liczba darmowych kopii: ",
+          controller: nip,
+        ),
         new RaisedButton(
           child: new Text("Dodaj!"),
           color: Colors.red,
@@ -48,7 +100,7 @@ class MyCard extends StatelessWidget {
   MyCard({this.label, this.controller});
 
   final String label;
-  final TextEditingController controller; 
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
@@ -59,11 +111,9 @@ class MyCard extends StatelessWidget {
         child: new Column(
           children: <Widget>[
             new TextField(
-            controller: controller,
-            decoration: new InputDecoration(
-              labelText: label
+              controller: controller,
+              decoration: new InputDecoration(labelText: label),
             ),
-          ),
           ],
         ),
       ),
