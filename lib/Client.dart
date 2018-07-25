5a380915560378bce34dc005a3aaeb9f2b87d78c
Kamil <kamil4525@gmail.com>
2018-07-25 18:04:41 +0200

    save client as JSON

--- lib/Client.dart
+++ lib/Client.dart
@@ -13,7 +13,17 @@ class Client {
   var tonerIncluded = false; // tonery wliczone w umowe
   var printerLease = false; // dzierzawa
 
-  Client(this.name, this.nip, this.contractPer, this.rate, this.deviceName, this.freeCopies, this.pagePrice, this.quaterRate, this.tonerIncluded, this.printerLease);
+  Client(
+      this.name,
+      this.nip,
+      this.contractPer,
+      this.rate,
+      this.deviceName,
+      this.freeCopies,
+      this.pagePrice,
+      this.quaterRate,
+      this.tonerIncluded,
+      this.printerLease);
 
   // debug
   void display() {
@@ -29,8 +39,28 @@ class Client {
     print(printerLease);
   }
 
-  void toJSON() {
-    
-  }
+  Client.fromJson(Map<String, dynamic> json)
+    : name = json['name'],
+      nip = json['nip'],
+      contractPer = json['contractPer'],
+      rate = json['rate'],
+      deviceName = json['deviceName'],
+      freeCopies = json['freeCopies'],
+      pagePrice = json['pagePrice'],
+      quaterRate = json['quaterRate'],
+      tonerIncluded = json['tonerIncluded'],
+      printerLease = json['printerLease'];
 
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'nip': nip,
+        'contractPer': contractPer,
+        'rate': rate,
+        'deviceName': deviceName,
+        'freeCopies': freeCopies,
+        'pagePrice': pagePrice,
+        'quaterRate': quaterRate,
+        'tonerIncluded': tonerIncluded,
+        'printerLease': printerLease
+      };
 }
--- lib/pages/NewClient.dart
+++ lib/pages/NewClient.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:elbiserwis/Client.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
 class NewClient extends StatelessWidget {
   @override
@@ -25,16 +28,68 @@ class NewClientState extends State<NewClientWidget> {
   TextEditingController freeCopies = new TextEditingController();
   TextEditingController pagePrice = new TextEditingController();
 
+  // JSON data
+  File jsonFile;
+  Directory dir;
+  String fileName = "clientsData.json";
+  bool fileExist = false;
+  Map<String, dynamic> fileContent;
+
   // checkbox values
   var quaterRate = false; // rozliczenie kwartalne (false -> miesieczne)
   var tonerIncluded = false; // tonery wliczone w umowe
   var printerLease = false; // dzierzawa
 
+  // JSON create & read
+  @override
+  void initState() {
+    super.initState();
+    getApplicationDocumentsDirectory().then((Directory directory) {
+      dir = directory;
+      jsonFile = new File(dir.path + "/" + fileName);
+      fileExist = jsonFile.existsSync();
+      if(fileExist) this.setState(() => fileContent = JSON.decode(jsonFile.readAsStringSync()));
+    });
+  }
+
+  void createFile(Map<String, dynamic> content, Directory dir, String fileName) {
+    print("Creating file!");
+    File file = new File(dir.path + "/" + fileName);
+    file.createSync();
+    fileExist = true;
+    file.writeAsStringSync(JSON.encode(content));
+  }
+
+  void writeToFile(Map<String, dynamic> content) {
+    print("Writing to file!");
+    if(fileExist) {
+      print("File exist!");
+      Map<String, dynamic> jsonFileContent = json.decode(jsonFile.readAsStringSync());
+      jsonFileContent.addAll(content);
+      jsonFile.writeAsStringSync(JSON.encode(jsonFileContent));
+    } else {
+      print("File does not exist!");
+      createFile(content, dir, fileName);
+    }
+    this.setState(() => fileContent = JSON.decode(jsonFile.readAsStringSync()));
+  }
+
+  // switches & buttons
   void onPressed() {
     setState(() {
+      // create a new client
       Client tomek = Client(name.text, nip.text, int.tryParse(contractPer.text)??0, int.tryParse(rate.text)??0, deviceName.text, int.tryParse(freeCopies.text)??0, double.tryParse(pagePrice.text)??0.0, quaterRate, tonerIncluded, printerLease);
       tomek.display();
       print("client has been created");
+      
+      // json test #temp
+      Map<String, dynamic> userMap = tomek.toJson();
+      var user = new Client.fromJson(userMap);
+      print(user.deviceName);
+      String jsonFile = json.encode(user);
+
+      // save to the db
+      writeToFile(userMap);
     });
   }
 
