927bdc9debc3529c57487e178e9cd121c0832535
Kamil <kamil4525@gmail.com>
2018-08-08 15:40:45 +0200

    firebase write & read json

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:elbiserwis/styles/MyColors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -16,6 +21,58 @@ class HomeWidget extends StatefulWidget {
 }
 
 class HomeState extends State<HomeWidget> {
+  // JSON data
+  File jsonFile;
+  Directory dir;
+  String fileName = "clientsData.json";
+  bool fileExist = false;
+  Map<String, dynamic> fileContent;
+
+  // JSON create & read
+  @override
+  void initState() {
+    super.initState();
+    getApplicationDocumentsDirectory().then((Directory directory) {
+      dir = directory;
+      jsonFile = new File(dir.path + "/" + fileName);
+      fileExist = jsonFile.existsSync();
+      if ((fileExist) && (this.mounted))
+        this.setState(
+            () => fileContent = JSON.decode(jsonFile.readAsStringSync()));
+    });
+  }
+
+  void createFile(
+      Map<String, dynamic> content, Directory dir, String fileName) {
+    print("Creating file!");
+    File file = new File(dir.path + "/" + fileName);
+    file.createSync();
+    fileExist = true;
+    file.writeAsStringSync(JSON.encode(content));
+  }
+
+  void writeAsNewFile(Map<String, dynamic> content) {
+    createFile(content, dir, fileName);
+    this.setState(() => fileContent = JSON.decode(jsonFile.readAsStringSync()));
+    print("A new file with data has been created!");
+  }
+
+  // save to Firebase
+  void saveData(File content) async {
+    var url = "https://elbiserwis-42e05.firebaseio.com/clients.json";
+    var httpClient = Client();
+    var response = await httpClient.post(url, body: JSON.encode(fileContent));
+    print("response=" + response.body);
+  }
+
+  // get data from Firebase
+  void loadData() async {
+    var url = "https://elbiserwis-42e05.firebaseio.com/clients.json";
+    var httpClient = Client();
+    var response = await httpClient.get(url);
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     // logo image
@@ -41,10 +98,42 @@ class HomeState extends State<HomeWidget> {
                     "elbiSerwis",
                     style: new TextStyle(fontSize: 20.0),
                   ),
+                  new RaisedButton(
+                    child: new Text("cumon"),
+                    onPressed: () {
+                      saveData(jsonFile);
+                    },
+                  ),
+                  new RaisedButton(
+                    child: new Text("pobierz"),
+                    onPressed: () {
+                      loadData();
+                    },
+                  )
                 ],
               ),
             )),
       ),
     );
   }
+
+  /*@override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("firebase")),
+      body: new StreamBuilder(
+          stream: Firestore.instance.collection('baby').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            return new ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                padding: const EdgeInsets.only(top: 10.0),
+                itemExtent: 25.0,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot ds = snapshot.data.documents[index];
+                  return new Text(" ${ds['name']} ${ds['votes']}");
+                });
+          }),
+    );
+  }*/
 }
